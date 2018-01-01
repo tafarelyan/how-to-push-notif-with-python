5efed979e1f6ac506aef4ba46bb05af394381c89
Tafarel Yan <tafarel.yan@gmail.com>
2018-01-01 11:44:19 -0200

    First commit - Working Script

---
+++ run.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+
+url = 'https://api.pushbullet.com/v2/pushes'
+headers = {
+    'Access-Token': 'YOUR-API-TOKEN',
+}
+
+payload = {
+    'type': 'note',
+    'title': 'Hello World',
+    'body': 'No content',
+}
+
+r = requests.post(url, headers=headers, json=payload)
+print(json.dumps(r.json(), indent=2))
